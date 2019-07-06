14ed71a7f39f962573276623f11a8d9d2b6476a8
Schow94 <Schow94@gmail.com>
2019-07-06 14:25:46 -0700

    Add Navbar and Footer to SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -29,7 +29,7 @@ export default class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showingAllColors } = this.props;
     const { format } = this.state;
 
     return (
@@ -37,18 +37,20 @@ export default class Navbar extends Component {
         <div className="logo">
           <Link to="/">reactcolorpicker</Link>
         </div>
-        <div className="slider-container">
-          <span>Level: {level}</span>
-          <div className="slider">
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-            />
+        {showingAllColors && (
+          <div className="slider-container">
+            <span>Level: {level}</span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="select-container">
           <Select value={format} onChange={this.onHandleChange}>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import ColorBox from './ColorBox';
+import PaletteFooter from './PaletteFooter';
 
 export default class SingleColorParent extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
     // console.log(this._shades);
+    this.state = { format: 'hex' };
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   gatherShades(palette, colorToFilterBy) {
     //returns all shades of a given color
     let shades = [];
     let allColors = palette.colors;
+    console.log(allColors);
 
     for (let key in allColors) {
       shades = shades.concat(
@@ -21,19 +26,27 @@ export default class SingleColorParent extends Component {
     return shades.slice(1);
   }
 
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+
   render() {
+    const { format } = this.state;
+    const { paletteName, emoji } = this.props.palette;
+
     const colorBoxes = this._shades.map(color => (
       <ColorBox
         key={color.id}
         name={color.name}
-        background={color.hex}
+        background={color[format]}
         showLink={false}
       />
     ));
     return (
       <div className="Palette">
-        <h1>Single Color Palette</h1>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className="Palette-colors">{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
