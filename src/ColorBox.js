88ada375f2dc1d21c6bc5ed4050107d4ccbbaf1c
Schow94 <Schow94@gmail.com>
2019-07-09 08:56:18 -0700

    Finish refactor of ColorBox CSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -23,6 +23,7 @@ const styles = {
     color: props =>
       chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'
   },
+  //if backgroundColor >= 0.7 set textColor to black, else white
   colorName: {
     color: props =>
       chroma(props.background).luminance() <= 0.08 ? 'white' : 'black'
@@ -62,6 +63,68 @@ const styles = {
     border: 'none',
     textDecoration: 'none',
     opacity: 0
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)'
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute'
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1 rem',
+      textTransform: 'uppercase'
+    },
+    '& p': {
+      fontSize: '1.5rem',
+      fontWeight: '100',
+      opacity: '0.8'
+    }
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: '0.4s ease-in-out',
+    transitionDelay: '0.3s'
   }
 };
 
@@ -92,20 +155,26 @@ class ColorBox extends Component {
     // console.log(chroma(background).luminance());
     //white is 1
     //black is 0
+    // console.log(chroma(this.props.background).luminance());
+    // console.log('no props', chroma(background).luminance());
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && 'show'}`}
+            className={`${classes.copyOverlay} ${copied &&
+              classes.showOverlay}`}
           />
-          <div className={`copy-msg ${copied && 'show'}`}>
+          <div
+            className={`${classes.copyMessage} ${copied &&
+              classes.showMessage}`}
+          >
             <h1>copied</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>
                 {name} {/*chroma(background).luminance()*/}
               </span>
