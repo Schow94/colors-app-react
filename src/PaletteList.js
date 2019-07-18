7ddaff87b2d5ac10eaa4794d92a357d8f7734b88
Schow94 <Schow94@gmail.com>
2019-07-18 07:39:06 -0700

    Fix dragging bug

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -52,7 +52,7 @@ class PaletteList extends Component {
   }
 
   render() {
-    const { palettes, classes, deletePalette } = this.props;
+    const { palettes, classes } = this.props;
     const { openDeleteDialog, deletingId } = this.state;
     return (
       <div className={classes.root}>
