dcd9e8548579f68e23425f409fbd6590b68d1922
Schow94 <Schow94@gmail.com>
2019-08-08 07:05:26 -0700

    Updates

---
+++ precache-manifest.606cf96d229998a6d8d29a8676b784b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4e3f13fd1e66d4f65e6d0c74e1e68e",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "1a945cb0a5c65207fd1d",
+    "url": "/colors-app/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "1aae6c3279b572c6f5aa",
+    "url": "/colors-app/static/css/main.9dfe3970.chunk.css"
+  },
+  {
+    "revision": "1a945cb0a5c65207fd1d",
+    "url": "/colors-app/static/js/2.b5f3a18a.chunk.js"
+  },
+  {
+    "revision": "1aae6c3279b572c6f5aa",
+    "url": "/colors-app/static/js/main.cb97d14f.chunk.js"
+  },
+  {
+    "revision": "658f8f8a04f3c7d8f86e",
+    "url": "/colors-app/static/js/runtime~main.7b7bc380.js"
+  },
+  {
+    "revision": "71d48285c17f4804ad02170b1b00f63d",
+    "url": "/colors-app/static/media/bg.71d48285.svg"
+  }
+]);
